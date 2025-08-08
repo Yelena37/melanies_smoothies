0db816162b8cc6a591725cb6c3d8d616efee595a
Yelena37 <ykolom@yahoo.com>
2025-08-08 10:44:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 import requests
-# import pandas
+import pandas
 
 # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
@@ -17,11 +17,9 @@ st.write(
   """
 )
 
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("Name on your Smoothie will be:", name_on_order)
  
-
 # option = st.selectbox (
 #     "How would you like to be contacted?",
 #     ("Email", "Home phone", "Mobile phone"))
@@ -31,7 +29,7 @@ st.write("Name on your Smoothie will be:", name_on_order)
 cnx = st.connection ("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))   
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
