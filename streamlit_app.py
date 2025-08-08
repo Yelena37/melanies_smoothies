368974c37789b2e8a728f7c7d75e3a708ea78559
Yelena37 <ykolom@yahoo.com>
2025-08-08 10:35:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop
+st.stop()
 
 # Convert a Snowpart dataframe to Pandas dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
